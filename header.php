eb3613716465731959043d4635566141affa7618
Shin-Fu <shinfu619@gmail.com>
2020-04-14 15:48:25 +0800

    finish header and footer

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Todo list</title>
+    <!--將bootstrap加進來 順序要放在前面,後面自己寫的main才會覆蓋掉它-->
+    <link rel="stylesheet" href="css/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <!--將jQuery和自己寫的js加進來-->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/action.js"></script>
+    <script src="js/handlebars-v4.7.3.js"></script>
+  </head>
+  <body>
