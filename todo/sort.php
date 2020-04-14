6f5dfd1000db59cdf50042eded162bb6e5d09f76
Shin-Fu <shinfu619@gmail.com>
2020-04-14 18:14:46 +0800

    fix sort API bug

--- todo/sort.php
+++ todo/sort.php
@@ -1,10 +1,10 @@
 <?php
 include ('../../db_todo.php');
 
-header('Content-Type: application/json; charset=utf-8'); 
+header('Content-Type: application/json; charset=utf-8');
 
 try {
-    $pdo = new PDO ("mysql:host=$db[host]; dbname=$db[dbname]; port=$db[port]; 
+    $pdo = new PDO ("mysql:host=$db[host]; dbname=$db[dbname]; port=$db[port];
     charset=$db[charset]",$db['username'],$db['password']);
 } catch(PDOException $e) {
     echo "Database Connection failed.";
@@ -12,13 +12,13 @@ try {
 }
 
 $sql = 'UPDATE todos SET `order`=:order WHERE id=:id';
-$statement = $pdo->prepare($sql);  
-foreach($_POST['orderpair'] as $key => $orderpair) {              
-    $statement->bindValue(':order', $_POST['order'], PDO::PARAM_STR);   
-    $statement->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+$statement = $pdo->prepare($sql);
+foreach($_POST['orderpair'] as $key => $orderpair) {
+    $statement->bindValue(':order', $orderpair['order'], PDO::PARAM_STR);
+    $statement->bindValue(':id', $orderpair['id'], PDO::PARAM_INT);
     $result = $statement -> execute();
 }
 
-if (!$result) {
+if ($result) {
     echo 'error';
-}                 
+}
